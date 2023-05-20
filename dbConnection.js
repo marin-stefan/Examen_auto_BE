cb1d78278555435e3afb519f64633142a2d18475
stefann06 <stefann06@yahoo.com>
2023-05-20 18:51:17 +0300

    initial commit

---
+++ dbConnection.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+mongoose.set("strictQuery", false);
+
+export class Database {
+    dbUri = "mongodb+srv://admin:utm123@";
+    connection = mongoose.connection;
+
+    constructor() {
+        try {
+            this.connection 
+                .on('connected', console.info.bind(console, 'Connectat la Baza de Date'))
+                .on('error', console.error.bind(console, 'Eroare conexiune: MongoDB' ))
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async connect() {
+        try {
+            await mongoose.connect(this.dbUri,
+                {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                }
+            );
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    async close() {
+        await this.connection.close();
+    } catch (error) {
+        console.log(error.message);
+
+    }
+}
+
---
+++ server.js
@@ -0,0 +1,40 @@
+import { Database } from "./dbConnection";
+import express from "express";
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+//routes
+// import xxxRoutes from "./src/routes/xxxRoutes.js"
+//
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+dotenv.config();
+
+let PORT = process.env.PORT || 8080;
+
+const db = new Database();
+db.connect();
+
+//
+// app.user('/xxx', xxxRoyutes)
+//
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use('/', express.static('static-app'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static-app/index.html'));
+})
+
+app.listen(PORT, () => console.log(`Aplicatia Backend asculta pe http://localhost:${PORT}`))
